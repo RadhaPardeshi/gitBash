cfde49a501934207c115a1a76561812ed6f095aa
RadhaPardeshi <radhadpardeshi@gmail.com>
2024-04-19 10:32:57 +0530

    Welcome to my project

---
+++ src/Demo/TestClass.java
@@ -0,0 +1,14 @@
+package Demo;
+
+public class TestClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Main Start");
+		int a=10;
+		int b=2;
+		int c=a/b;
+		System.out.println(c);
+		System.out.println("Main end");
+	}
+
+}
